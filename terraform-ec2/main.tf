be6791e6be5a89e388f5dc3e3dad9b2cb92bbd45
abdoudrammeh6565@gmail.com <abdoudrammeh6565@gmail.com>
2026-04-02 22:51:16 +0200

    update the ports

--- terraform-ec2/main.tf
+++ terraform-ec2/main.tf
@@ -130,6 +130,18 @@ resource "azurerm_network_security_group" "main" {
 }
 
   security_rule {
+    name                       = "allow-socat-frontend"
+    priority                   = 1005
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8888"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
     name                       = "allow-grafana-nodeport"
     priority                   = 1009
     direction                  = "Inbound"
